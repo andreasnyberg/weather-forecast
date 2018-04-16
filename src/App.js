9412f022d876b3c47d61e8071a0266fa33991b84
Andreas <chas@Chass-MBP.guest.int.chas.se>
2018-04-16 15:11:32 +0200

    tezzzt

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <li> sätt upp actions, reducers etc (kolla i weather-forecast-error)</li>
           <li>kolla status i browsern ofta, så samma error kan lösas om det uppstår!</li>
           <li> have fuuuuuun</li>
+          <li> have fuuuuuun git tezzzt ba!</li>
         </ol>
       </div>
     );
