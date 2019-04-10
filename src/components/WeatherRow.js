1e8b9f44bae8676c462102c144495d4e533b2e4b
Andreas Nyberg <andreas.nyberg@chas.se>
2019-04-10 15:55:11 +0200

    Toggle hour-rows on click

--- src/components/WeatherRow.js
+++ src/components/WeatherRow.js
@@ -1,28 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import WeatherRowHour from './WeatherRowHour';
 
-const WeatherRow = (props) => {
-  const hours = props.data.hours.map((item, i) => <WeatherRowHour data={item} key={i} />);
+class WeatherRow extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div>
-      <div className="row row--day">
-        <div className="item">
-          <p>{props.data.day}</p>
+    this.state = {
+      isOpen: false
+    };
+
+    this.handleRowClick = this.handleRowClick.bind(this);
+  }
+
+  handleRowClick() {
+    this.setState(prevState => ({
+      isOpen: !prevState.isOpen
+    }));
+  }
+
+  renderWeatherRowHours() {
+    return this.props.data.hours.map((item, i) => <WeatherRowHour data={item} key={i} />);
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="row row--day" onClick={this.handleRowClick}>
+          <div className="item">
+            <p>{this.props.data.day}</p>
+          </div>
+
+          <div className="item">
+            <p>{this.props.data.tempMin}</p>
+          </div>
+
+          <div className="item">
+            <p>{this.props.data.tempMax}</p>
+          </div>
         </div>
 
-        <div className="item">
-          <p>{props.data.tempMin}</p>
-        </div>
-
-        <div className="item">
-          <p>{props.data.tempMax}</p>
-        </div>
+        {this.state.isOpen &&
+          this.renderWeatherRowHours()
+        }
       </div>
-
-      { hours }
-    </div>
-  );
+    );
+  }
 };
 
 export default WeatherRow;
